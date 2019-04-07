3bed30baf5d3f82b89334769b0eb299ec80e9d8c
ichiya <kazuya7187@gmail.com>
2019-04-07 16:15:20 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.24e22ea251c1156f2829486086de4b58.js"
+  "precache-manifest.4224b5f0c90ad5f54b7fc2b9a7f91fd5.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vuetify-template"});
