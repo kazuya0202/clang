ea984d5abf5a403b0af204c8733a0faaa6ad3226
ichiya <kazuya7187@gmail.com>
2019-04-28 18:43:33 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,21 @@ import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store';
 
-// tslint:disable-next-line:no-var-requires
+// tslint:disable
+// Scroll
 const scrollTo = require('vue-scrollto');
 Vue.use(scrollTo);
 
+// Syntax Highlight
 // vs dark-mode (some change)
-import 'highlight.js/styles/vs2015-decoration.css';
-// tslint:disable-next-line:no-var-requires
-const cpp = require('highlight.js/lib/languages/cpp.js');
-// tslint:disable-next-line:no-var-requires
-const javascript = require('highlight.js/lib/languages/javascript.js');
-
 import VueHighlightJS from 'vue-highlight.js';
+
+import 'highlight.js/styles/vs2015-decoration.css';
+const cpp = require('highlight.js/lib/languages/cpp.js');
+const javascript = require('highlight.js/lib/languages/javascript.js');
 Vue.use(VueHighlightJS, { languages: { cpp, javascript } });
 
+// styles
 import './components/stylus-pane/main.styl';
 
 Vue.config.productionTip = false;
